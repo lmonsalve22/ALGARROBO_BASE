e61f00e3c924fc28b3be114a0ee10c384b88972f
lmonsalve22 <lmonsalve22@gmail.com>
2026-02-10 18:35:17 -0300

    Update app21.py

--- backend/app21.py
+++ backend/app21.py
@@ -10,6 +10,7 @@ import bcrypt
 import logging
 import threading
 from flask import Flask, request, jsonify, send_file
+from werkzeug.utils import secure_filename
 from flask_cors import CORS
 from datetime import datetime, timedelta
 from functools import wraps
